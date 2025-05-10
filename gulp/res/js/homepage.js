3dc94f31aa7870d6ab2646f12bde2116e4889493
s5cubed <s5s5s5wum@gmail.com>
2025-05-10 17:26:41 +0100

    Added event load thingy

--- gulp/res/js/homepage.js
+++ gulp/res/js/homepage.js
@@ -1,8 +1,10 @@
-if (document.getElementsByClassName('home-image-widget').length > 0) {
-	var imgclickwidget = document.getElementsByClassName('home-image-widget');
-	for (i = 0;i < imgclickwidget.length;i++) {
-		imgclickwidget[i].onclick = function() {
-			this.src = this.src + '?t=' + new Date().getTime();
-		};
+window.addEventListener('DOMContentLoaded', () => {
+	if (document.getElementsByClassName('home-image-widget').length > 0) {
+		var imgclickwidget = document.getElementsByClassName('home-image-widget');
+		for (i = 0;i < imgclickwidget.length;i++) {
+			imgclickwidget[i].onclick = function() {
+				this.src = this.src + '?t=' + new Date().getTime();
+			};
+		}
 	}
-}
+});
